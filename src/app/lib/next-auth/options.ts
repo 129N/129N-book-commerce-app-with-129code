8e48f486e8fd0a4ead58a926e3bd1723f3b2e716
129N <i.nakano@nipponcompass.asia>
2025-07-14 18:47:42 +0200

    updated

---
+++ src/app/lib/next-auth/options.ts
@@ -0,0 +1,32 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { Prisma } from "@prisma/client";
+import { Session } from "inspector/promises";
+
+export const nextAuthOptions: NextAuthOptions= {
+    //ログインをいろいろなプロバイダーでできる。
+    debug: false,
+    providers:[
+        GithubProvider({
+      clientId: process.env.GITHUB_ID!, //IDがundefinedの場合がある
+      clientSecret: process.env.GITHUB_SECRET!, //上記同文
+    }),
+
+    ],
+
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+
+        //sign inからFEに値を渡す仕組み callback関数
+        session: ({session, user})=> {
+            return{
+                ...session,
+                user:{
+                    ...session.user,
+                    id: user.id,
+                },
+            };
+        },
+    },
+};
